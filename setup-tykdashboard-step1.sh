bea2decbd76d4822a61cf3bc9d33363b387419ee
Joshua Ben-Tzion Quek <joven2468@gmail.com>
2018-06-04 10:46:24 +0800

    Change $HOSTNAME to $EC2PUBLICIPADDRESS

--- setup-tykdashboard-step1.sh
+++ setup-tykdashboard-step1.sh
@@ -78,7 +78,7 @@ sudo service mongod start
 
 sudo service redis start
 
-sudo /opt/tyk-dashboard/install/setup.sh --listenport=3000 --redishost=localhost --redisport=6379 --mongo=mongodb://127.0.0.1/tyk_analytics --tyk_api_hostname=$HOSTNAME --tyk_node_hostname=http://localhost --tyk_node_port=8080--portal_root=/portal --domain="$EC2PUBLICIPADDRESS"
+sudo /opt/tyk-dashboard/install/setup.sh --listenport=3000 --redishost=localhost --redisport=6379 --mongo=mongodb://127.0.0.1/tyk_analytics --tyk_api_hostname=$EC2PUBLICIPADDRESS --tyk_node_hostname=http://localhost --tyk_node_port=8080--portal_root=/portal --domain="$EC2PUBLICIPADDRESS"
 
 sudo service tyk-dashboard start
 
