fbe731f26a6b36a661ed8b89087843fb0939b960
Joshua Quek <joshua.quek@accenture.com>
2018-06-04 18:02:27 +0800

    Renamed Scripts

---
+++ 2-dashboard-bootstrap.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Do this after you have completed `1-dashboard-bootstrap.sh` and added the license on the tyk-dashboard `http://localhost:3000`
+
+sudo /opt/tyk-dashboard/install/bootstrap.sh tykbootstrappeduser
+
